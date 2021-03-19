d9936c0440802f8caa7552cedfe888848a57c26e
sajclarke <shannonajclarke@gmail.com>
2021-03-19 11:40:28 -0400

    chore: resolve minor UI styling issues

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Map from "./components/Map";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-gray-300">
+    <div className="w-full flex flex-col h-screen bg-gray-300">
       <Header title="Pothole App" />
 
-      <div className="h-full w-full flex bg-red-500">
+      <div className="w-full flex-1 flex">
         <div className="w-1/6 bg-white">
           <p>Sidebar</p>
         </div>
-        <div className="w-5/6 bg-white">
+        <div className="w-5/6">
           <Map />
         </div>
       </div>

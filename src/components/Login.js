d6b520a2c6c063abb3817882e7b637bac538a8f0
unknown <cheney.antoine@gmail.com>
2021-03-22 23:29:28 -0400

    chore: add react-router

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import {Link} from 'react-router-dom';
+
+
+
+const Login = () => {   
+
+   
+
+    return (
+        <div >
+           Login
+        </div>
+        
+    );
+}
+export default Login
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import Header from "../components/Header";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 
@@ -46,11 +47,24 @@ const Map = () => {
   }, []);
 
   return (
-    <div className="h-full w-full" ref={mapContainer}>
+    <div className="w-full flex flex-col h-screen bg-gray-300">
+      <Header title="Pothole App" />
+
+      <div className="w-full flex-1 flex">
+        <div className="w-1/6 bg-white">
+          <p>Sidebar</p>
+        </div>
+        <div className="w-5/6">
+         <div className="h-full w-full" ref={mapContainer}>
       <div className="absolute m-5 z-10 rounded bg-gray-800 bg-opacity-80 p-2 text-white">
         Longitude: {mapCenter.lng} | Latitude: {mapCenter.lat} | Zoom: {zoom}
       </div>
     </div>
+        </div>
+      </div>  
+      </div>
+
+    
   );
 };
 

568569710a2b714de0a3ba9e49715da15141969b
unknown <cheney.antoine@gmail.com>
2021-03-31 10:32:54 -0400

    feat: add display potholes on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,29 +2,69 @@ import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Header from "../components/Header";
+import firebase from '../auth/Firebase';
+
+
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
-
 const Map = () => {
   const mapContainer = useRef();
+  const [data, setData] = useState([])
+  const [lat, setLat] = useState([])
+  let map = null;
+
+  const ref = firebase.firestore().collection('issues');
+
+  useEffect(() => {
+
+    ref.onSnapshot(snapshot => {
+      const retrieve = []
+      snapshot.docs.forEach(doc => {
+
+        retrieve.push({ ...doc.data(), id: doc.id })
+
+      })
+      setData(retrieve)
+
+      retrieve.map((point) => {
+        new mapboxgl.Marker()
+          .setPopup(new mapboxgl.Popup().setHTML('<div>' + point.Location + '</div><p>' + point.IssueDesc + '</p>')) // add popup
+          .setLngLat([point.Longitude, point.Latitude])
+          .addTo(map)
+      })
+
+      return () => ref()
+
+    });
+  }, [])
+
+
   const [mapCenter, setMapCenter] = useState({
     lat: -59.570655, //Lng center for Barbados
     lng: 13.193114, //Lat center for Barbados
   });
   const [zoom, setZoom] = useState(9);
 
+  const geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true
+    },
+    trackUserLocation: true
+  });
+
   useEffect(() => {
     var bounds = [
       [-60.27172006347844, 12.729393667108312], // Southwest coordinates
       [-58.86958993652529, 13.655956198937346], // Northeast coordinates
     ];
-    const map = new mapboxgl.Map({
+    map = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [mapCenter.lng, mapCenter.lat],
       zoom: zoom,
       maxBounds: bounds,
     });
+
 
     // Add zoom and rotation controls to the map.
     map.addControl(new mapboxgl.NavigationControl());
@@ -34,17 +74,39 @@ const Map = () => {
       [-59.8920050976494, 12.958016032319861],
     ]);
 
-    map.on("move", () => {
+    map.on("move", (e) => {
       setMapCenter({
         lng: map.getCenter().lng.toFixed(4),
         lat: map.getCenter().lat.toFixed(4),
+
       });
       setZoom(map.getZoom().toFixed(2));
     });
 
+    map.addControl(geolocate);
+    // Set an event listener that fires
+    // when a geolocate event occurs.
+    geolocate.on('geolocate', function () {
+      console.log('A geolocate event has occurred.')
+    });
+
+    map.on('click', function (e) {
+      var features = map.queryRenderedFeatures(e.point, {
+
+      });
+
+      if (!features.length) {
+        return;
+      }
+
+    });
+
+
     return () => map.remove();
     //eslint-disable-next-line
   }, []);
+
+
 
   return (
     <div className="w-full flex flex-col h-screen bg-gray-300">
@@ -53,18 +115,20 @@ const Map = () => {
       <div className="w-full flex-1 flex">
         <div className="w-1/6 bg-white">
           <p>Sidebar</p>
+
         </div>
         <div className="w-5/6">
-         <div className="h-full w-full" ref={mapContainer}>
-      <div className="absolute m-5 z-10 rounded bg-gray-800 bg-opacity-80 p-2 text-white">
-        Longitude: {mapCenter.lng} | Latitude: {mapCenter.lat} | Zoom: {zoom}
+          <div className="h-full w-full" ref={mapContainer}>
+            <div className="absolute m-5 z-10 rounded bg-gray-800 bg-opacity-80 p-2 text-white">
+              Longitude: {mapCenter.lng} | Latitude: {mapCenter.lat} | Zoom: {zoom}
+            </div>
+
+          </div>
+        </div>
       </div>
     </div>
-        </div>
-      </div>  
-      </div>
 
-    
+
   );
 };
 
